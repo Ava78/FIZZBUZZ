358ffbdff9ec4496a2fbe9acd63e73213bb52c61
apprenti <you@example.comclear>
2015-10-28 14:36:34 +0100

    Mise à jour Quete FizzBuzz

--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ require_relative './fizzbuzz.rb'
 class FizzBuzz < MiniTest::Test
 
 	def test_pour_3
-	    assert_equal 'fizz', 3.fizzbuzz
+	    assert_equal 'fizz', 3.fizzbuzz(3)
 	end
 
 	def test_pour_5
